fa25febf0503d838041554bbb1fb142ceeaeda7e
Fermin Pozo <ferminpozo2k@gmail.com>
2022-10-29 13:25:21 +0200

    1 commit rama Fermin

---
+++ EnoReg/AñadirSalidas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnoReg
+{
+    public partial class AñadirSalida : Form
+    {
+
+        public AñadirSalida()
+        {
+            InitializeComponent();
+            this.Font = Properties.Settings.Default.Font;
+            this.BackColor = Properties.Settings.Default.ColorFondo;
+            this.ForeColor = Properties.Settings.Default.ColorLetra;
+        }
+    }
+}
