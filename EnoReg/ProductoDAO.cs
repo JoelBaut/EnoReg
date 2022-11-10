1e86bda7758f21b51a9c6d1151be533cc490615b
joelBaut <joelbautistacn@gmail.com>
2022-11-10 20:28:59 +0100

    Actualizacion Inserciones de entradas.

--- EnoReg/ProductoDAO.cs
+++ EnoReg/ProductoDAO.cs
@@ -25,16 +25,22 @@ namespace EnoReg
         public void cerrarConexion() {
             conexionDB.cerrarConexion();
         }
-        public MySqlDataReader CargarTodo() {
-            String sql = "select fecha_entrada Fecha, nombre Nombre, proveedor Proveedor, lote Lote, fecha_caducidad Caducidad, albaran Albaran,cantidad Entrada,'-' Salida, stock Stock, '-' Destino, '-' Observaciones" +
+        public MySqlDataReader CargarTodo()
+        {
+            String sql = "select fecha_entrada Fecha, nombre Nombre, proveedor Proveedor, lote Lote, fecha_caducidad Caducidad, albaran Albaran,FORMAT(cantidad,3) Entrada,'-' Salida, FORMAT(stock,3) Stock, '-' Destino, '-' Observaciones" +
                 " from producto_entrada, producto" +
                 " where producto_entrada.id_producto = producto.id_producto" +
                 " Union" +
-                " select fecha_salida Fecha, nombre Nombre, '-' Proveedor, lote Lote, '-'Caducidad, '-'Albaran,'-'Entrada,cantidad Salida, stock Stock, destino Destino, observaciones Observaciones" +
+                " select fecha_salida Fecha, nombre Nombre, '-' Proveedor, lote Lote, '-'Caducidad, '-'Albaran,'-'Entrada,FORMAT(cantidad,3) Salida, FORMAT(stock,3) Stock, destino Destino, observaciones Observaciones" +
                 " from producto_salida, producto" +
                 " where producto_salida.id_producto = producto.id_producto" +
                 " order by fecha DESC;";
             return conexionDB.Select(sql);
+        }
+        public void ObtenerUnidad(String nombreProducto)
+        {
+            String sql = "select unidad from producto where nombre='" + nombreProducto + "'";
+            // conexionDB.
         }
 
         public void InsertarProducto(String nombre, String unidad, byte[] image)
@@ -44,11 +50,12 @@ namespace EnoReg
             
             conexionDB.InsertarProducto(sql,image);
         }
-        public void InsertarEntrada() {
+        public void InsertarEntrada(String nombre,string fecha,String lote, String Albaran, String proveedor, string fcadudidad,String cantidad ) {
 
-            String sql = "INSERT INTO `producto_entrada`(`id_producto`, `fecha_entrada`, `lote`, `albaran`, `proveedor`, `fecha_caducidad`, `cantidad`) VALUES ('[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]');";
-
+            String sql = "INSERT INTO `producto_entrada`(`id_producto`, `fecha_entrada`, `lote`, `albaran`, `proveedor`, `fecha_caducidad`, `cantidad`) VALUES ((SELECT id_producto from producto WHERE nombre='"+ nombre + "'),'"+ fecha+"','"+lote+ "','"+ Albaran + "','"+ proveedor + "','"+ fcadudidad + "','"+ cantidad + "');";
             conexionDB.Insertar(sql);
+            sql = "Update producto set stock = stock + "+ cantidad + " where id_producto = (SELECT id_producto from producto WHERE nombre='" + nombre + "')";
+            conexionDB.Update(sql);
         }
         public void InsertarSalida()
         {
